f321572a357beb022ff23f43239a84ce288d0749
Mohd Ashif <mohdashif0770@gmail.com>
2025-09-08 00:31:40 +0530

    Fixed root path

--- app.js
+++ app.js
@@ -81,9 +81,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get("/", (req, res) => {
-  res.send(`Welcome to home page `);
-});
+
 
 app.get("/fakeUser", async (req, res) => {
   const user = new User({
