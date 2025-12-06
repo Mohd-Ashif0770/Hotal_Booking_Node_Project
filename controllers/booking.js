52048101549271ab0e32b77fc874e044b80d4650
Mohd Ashif <mohdashif0770@gmail.com>
2025-12-06 23:58:23 +0530

    Booking cancel feature added

--- controllers/booking.js
+++ controllers/booking.js
@@ -2,7 +2,7 @@
 const Booking = require("../models/booking");
 const Listing = require("../models/listing");
 
-// show booking form
+//! show booking form
 module.exports.renderBookingForm = async (req, res) => {
   const listing = await Listing.findById(req.params.id);
   if (!listing) {
@@ -12,7 +12,7 @@ module.exports.renderBookingForm = async (req, res) => {
   res.render("bookings/new", { listing });
 };
 
-// Handle booking submision
+//! Handle booking submision
 module.exports.createBooking = async (req, res) => {
   const { startDate, endDate } = req.body;
 
@@ -27,41 +27,14 @@ module.exports.createBooking = async (req, res) => {
     listing: listing._id,
     startDate,
     endDate,
-    status: "pending"
+    status: "pending",
   });
 
   req.flash("success", "Booking created successfully");
-  // res.redirect(`/bookings/${booking._id}`);
   res.redirect(`/my-bookings`);
-  // res.send("Booking created");
 };
 
-// CANCEL booking
-module.exports.cancelBooking = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const booking = await Booking.findById(id);
-
-    if (!booking) {
-      req.flash("error", "Booking not found");
-      return res.redirect("/bookings");
-    }
-
-    if (!booking.user.equals(req.user._id)) {
-      req.flash("error", "You are not authorized to cancel this booking");
-      return res.redirect("/bookings");
-    }
-
-    await Booking.findByIdAndDelete(id);
-    req.flash("success", "Booking cancelled successfully");
-    res.redirect("/bookings");
-  } catch (err) {
-    console.error(err);
-    req.flash("error", "Something went wrong while cancelling booking");
-    res.redirect("/bookings");
-  }
-};
-
+//! Fetch all Bookings
 module.exports.showBookings = async (req, res) => {
   const userId = req.user._id;
 
@@ -72,12 +45,83 @@ module.exports.showBookings = async (req, res) => {
   res.render("bookings/index", { bookings });
 };
 
-module.exports.viewBooking= async(req, res)=>{
-  const {id}=req.params;
-  const booking= await Booking.findById(id).populate('listing').populate('user');
-   if (!booking) {
+//! View Booking
+module.exports.viewBooking = async (req, res) => {
+  const { id } = req.params;
+  const booking = await Booking.findById(id)
+    .populate("listing")
+    .populate("user");
+  if (!booking) {
     req.flash("error", "Booking not found");
     return res.redirect("/my-bookings");
   }
-  res.render('bookings/show', {booking});
+  res.render("bookings/show", { booking });
+};
+
+
+//! Edit Booking
+module.exports.renderEditBooking = async (req, res) => {
+  const booking = await Booking.findById(req.params.id);
+  res.render("bookings/editBooking", { booking });
+};
+
+module.exports.editBooking = async (req, res) => {
+  const { startDate, endDate } = req.body;
+
+  if (startDate >= endDate) {
+    req.flash("error", "Booking Date should be valid");
+    return res.redirect(`/my-bookings/${req.params.id}/edit`);
+  }
+
+  const booking = await Booking.findByIdAndUpdate(req.params.id, {
+    startDate,
+    endDate,
+  });
+
+  if (!booking) {
+    req.flash("error", "Booking not found");
+    return res.redirect("/my-bookings");
+  }
+
+  req.flash("success", "Booking updated successfully");
+  return res.redirect(`/my-bookings/${req.params.id}`);
+};
+
+//! Cancel Booking
+module.exports.cancelBooking=async(req,res)=>{
+  try{
+    const booking= await Booking.findByIdAndDelete(req.params.id);
+    req.flash("success", "Booking canceled successfully");
+    res.redirect(`/my-bookings`);
+
+  }catch(err){
+    res.flash("error", "Error when canceling booking")
+    return res.redirect(`/my-bookings/${req.params.id}`);
+  }
 }
+
+// CANCEL booking
+// module.exports.cancelBooking = async (req, res) => {
+//   try {
+//     const { id } = req.params;
+//     const booking = await Booking.findById(id);
+
+//     if (!booking) {
+//       req.flash("error", "Booking not found");
+//       return res.redirect("/my-bookings");
+//     }
+
+//     // if (!booking.user.equals(req.user._id)) {
+//     //   req.flash("error", "You are not authorized to cancel this booking");
+//     //   return res.redirect("/bookings");
+//     // }
+
+//     await Booking.findByIdAndDelete(id);
+//     req.flash("success", "Booking cancelled successfully");
+//     res.redirect("/my-bookings");
+//   } catch (err) {
+//     console.error(err);
+//     req.flash("error", "Something went wrong while cancelling booking");
+//     res.redirect("/my-bookings");
+//   }
+// };
--- routes/bookingRoute.js
+++ routes/bookingRoute.js
@@ -1,14 +1,17 @@
 // routes/bookings.js
 const express = require("express");
 const router = express.Router();
-const { renderBookingForm, createBooking, cancelBooking,showBookings, viewBooking } = require("../controllers/booking");
+const { renderBookingForm, createBooking,showBookings, viewBooking , editBooking,cancelBooking, renderEditBooking} = require("../controllers/booking");
 const { isLoggedIn } = require("../middlewares");
 
 router.get("/listings/:id/book", isLoggedIn, renderBookingForm);
 router.post("/listings/:id/book", isLoggedIn, createBooking);
-// router.post("/listings/:id/cancel", isLoggedIn, cancelBooking);
 
 router.get("/my-bookings", isLoggedIn, showBookings)
 router.get("/my-bookings/:id", isLoggedIn, viewBooking)
+
+router.get("/my-bookings/:id/edit", isLoggedIn, renderEditBooking)
+router.put("/my-bookings/:id", isLoggedIn, editBooking)
+router.delete("/my-bookings/:id", isLoggedIn, cancelBooking)
 
 module.exports = router;
